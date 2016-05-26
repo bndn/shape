35c6b53b519e1c1ff31f35edfd86832b501fd89f
AndreasHassing <andreas@famhassing.dk>
2016-05-26 16:03:08 +0200

    Add Mesh documentation

--- src/Shape/Mesh.fs
+++ src/Shape/Mesh.fs
@@ -4,6 +4,15 @@ module Shape.Mesh
 open Meshly
 open Texture
 
+/// <summary>
+/// Compute an intersection between a ray and a mesh.
+/// </summary>
+/// <param name=m>Mesh to check intersection with.</param>
+/// <param name=r>Ray to check intersection with.</param>
+/// <param name=t>Texture to return material of.</param>
+/// <returns>
+/// A single intersection when the ray hits, or none when the ray misses.
+/// </returns>
 let intersect m r t =
     match Meshly.getIntersection r m with
     | Some i ->
@@ -15,8 +24,19 @@ let intersect m r t =
         [Hit(d, n, m)]
     | None -> []
 
+/// <summary>
+/// Get the bounds of a mesh.
+/// </summary>
+/// <param name=m>Mesh to get bounds of.</param>
+/// <returns>The bounds of a mesh.</returns>
 let bounds m = Meshly.getBounds m |> Some
 
+/// <summary>
+/// Create a triangle mesh from a PLY-formatted string.
+/// </summary>
+/// <param name=s>The input string.</param>
+/// <param name=n>Define if the normals are flat- or smooth shaded.</param>
+/// <returns>The created shape.</returns>
 let make s n =
     let n = if n then Meshly.Smooth else Meshly.Flat
     let m = Meshly.make s n
